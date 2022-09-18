b6bd646352c16f8c5d49a307f84227000e94a37b
matemaptichka <nasty.internal@gmail.com>
2022-09-18 20:04:11 +0300

    fixes

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,46 +1,63 @@
 package ru.netology.radio;
 
 public class Radio {
-    public int CurrentStation;
-    public int Volume;
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+    private int currentStation;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private int volume;
 
     public void next() {
-        if (CurrentStation >= 9) {
-            CurrentStation = 0;
+        if (currentStation >= 9) {
+            currentStation = 0;
             return;
         }
-        CurrentStation += 1;
+        currentStation += 1;
     }
 
     public void prev() {
-        if (CurrentStation <= 0) {
-            CurrentStation = 9;
+        if (currentStation <= 0) {
+            currentStation = 9;
             return;
         }
-        CurrentStation -= 1;
+        currentStation -= 1;
     }
 
     public void SetCurrentStation(int Station) {
         if (Station < 0 | Station > 9) {
             return;
         }
-        CurrentStation = Station;
+        currentStation = Station;
     }
 
     public void plus() {
-        if (Volume >= 10) {
-            Volume = 10;
+        if (volume >= 10) {
+            volume = 10;
             return;
         }
-        Volume += 1;
+        volume += 1;
     }
 
     public void minus() {
-        if (Volume <= 0) {
-            Volume = 0;
+        if (volume <= 0) {
+            volume = 0;
             return;
         }
-        Volume -= 1;
+        volume -= 1;
     }
 }
 
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -7,152 +7,152 @@ public class RadioTest {
     @Test
     public void testNext() {
         Radio radio = new Radio();
-        radio.CurrentStation = 8;
+        radio.setCurrentStation(8);
         radio.next();
-        Assertions.assertEquals(9, radio.CurrentStation);
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void testNextMax() {
         Radio radio = new Radio();
-        radio.CurrentStation = 9;
+        radio.setCurrentStation(9);
         radio.next();
-        Assertions.assertEquals(0, radio.CurrentStation);
+        Assertions.assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     public void testNextMoreMax() {
         Radio radio = new Radio();
-        radio.CurrentStation = 10;
+        radio.setCurrentStation(10);
         radio.next();
-        Assertions.assertEquals(0, radio.CurrentStation);
+        Assertions.assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     public void testPrev() {
         Radio radio = new Radio();
-        radio.CurrentStation = 1;
+        radio.setCurrentStation(1);
         radio.prev();
-        Assertions.assertEquals(0, radio.CurrentStation);
+        Assertions.assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     public void testPrevMin() {
         Radio radio = new Radio();
-        radio.CurrentStation = 0;
+        radio.setCurrentStation(0);
         radio.prev();
-        Assertions.assertEquals(9, radio.CurrentStation);
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void testPrevLessMin() {
         Radio radio = new Radio();
-        radio.CurrentStation = -1;
+        radio.setCurrentStation(-1);
         radio.prev();
-        Assertions.assertEquals(9, radio.CurrentStation);
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void testSet() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(5);
-        Assertions.assertEquals(5, radio.CurrentStation);
+        Assertions.assertEquals(5, radio.getCurrentStation());
     }
 
     @Test
     public void testSetLessMax() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(8);
-        Assertions.assertEquals(8, radio.CurrentStation);
+        Assertions.assertEquals(8, radio.getCurrentStation());
     }
 
     @Test
     public void testSetMax() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(9);
-        Assertions.assertEquals(9, radio.CurrentStation);
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
 
     @Test
     public void testSetMoreMax() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(10);
-        Assertions.assertEquals(3, radio.CurrentStation);
+        Assertions.assertEquals(3, radio.getCurrentStation());
     }
 
     @Test
     public void testSetMoreMin() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(1);
-        Assertions.assertEquals(1, radio.CurrentStation);
+        Assertions.assertEquals(1, radio.getCurrentStation());
     }
 
     @Test
     public void testSetMin() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(0);
-        Assertions.assertEquals(0, radio.CurrentStation);
+        Assertions.assertEquals(0, radio.getCurrentStation());
     }
 
     @Test
     public void testSetLessMin() {
         Radio radio = new Radio();
-        radio.CurrentStation = 3;
+        radio.setCurrentStation(3);
         radio.SetCurrentStation(-1);
-        Assertions.assertEquals(3, radio.CurrentStation);
+        Assertions.assertEquals(3, radio.getCurrentStation());
     }
 
     @Test
     public void testPlus() {
         Radio radio = new Radio();
-        radio.Volume = 9;
+        radio.setVolume(9);
         radio.plus();
-        Assertions.assertEquals(10, radio.Volume);
+        Assertions.assertEquals(10, radio.getVolume());
     }
 
     @Test
     public void testPlusMax() {
         Radio radio = new Radio();
-        radio.Volume = 10;
+        radio.setVolume(10);
         radio.plus();
-        Assertions.assertEquals(10, radio.Volume);
+        Assertions.assertEquals(10, radio.getVolume());
     }
 
     @Test
     public void testPlusMoreMax() {
         Radio radio = new Radio();
-        radio.Volume = 11;
+        radio.setVolume(11);
         radio.plus();
-        Assertions.assertEquals(10, radio.Volume);
+        Assertions.assertEquals(10, radio.getVolume());
     }
 
     @Test
     public void testMinus() {
         Radio radio = new Radio();
-        radio.Volume = 1;
+        radio.setVolume(1);
         radio.minus();
-        Assertions.assertEquals(0, radio.Volume);
+        Assertions.assertEquals(0, radio.getVolume());
     }
 
     @Test
     public void testMinusMin() {
         Radio radio = new Radio();
-        radio.Volume = 0;
+        radio.setVolume(0);
         radio.minus();
-        Assertions.assertEquals(0, radio.Volume);
+        Assertions.assertEquals(0, radio.getVolume());
     }
 
     @Test
     public void testMinusLessMin() {
         Radio radio = new Radio();
-        radio.Volume = -1;
+        radio.setVolume(-1);
         radio.minus();
-        Assertions.assertEquals(0, radio.Volume);
+        Assertions.assertEquals(0, radio.getVolume());
     }
 }
